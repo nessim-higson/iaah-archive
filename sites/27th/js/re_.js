0207ad25823bcf6ee9a3d8d8a5099cec03466e1c
Nessim Higson <ness@iamalwayshungry.com>
2026-07-12 00:11:51 +0200

    Port iamthe27thletter.com into the museum as new 'experiments' wing (click-drag canvas, tracker neutralized)

---
+++ sites/27th/js/re_.js
@@ -0,0 +1,4 @@
+// Original site used ReInvigorate analytics (stats.reinvigorate.net, defunct).
+// Neutralized for the archive: no external tracking pixel, no console errors.
+function re_(){}
+function visita(){}
